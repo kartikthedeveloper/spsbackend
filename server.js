41fe6244334a9a5ec3d11be96eb0d3d1daf30de8
kartikthedeveloper <kartik.thedeveloper@gmail.com>
2026-09-21 21:25:41 +0530

    Restrict CORS to specific client URL

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 
 connectDB();
 
-app.use(cors({ origin: process.env.CLIENT_URL || '*', credentials: true }));
+app.use(cors({ origin: 'https://successpointsikar.netlify.app', credentials: true }));
 app.use(express.json({ limit: '5mb' }));
 app.use(morgan('dev'));
 
